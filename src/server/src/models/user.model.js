6eeea021a974209f1da7d2a8ec38d6899a92e252
Alexander King Perocho <perocho.xela@gmail.com>
2024-06-02 15:53:00 -0400

    added pfp, admin, timestamp to schema

--- src/server/src/models/user.model.js
+++ src/server/src/models/user.model.js
@@ -1,22 +1,34 @@
 import mongoose from 'mongoose';
 
-export default new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-        maxLength: 20,
-        minLength: 3,
+export default new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            maxLength: 20,
+            minLength: 3,
+        },
+        password: {
+            type: String,
+            required: true,
+            maxLength: 20,
+            minLength: 8,
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+        },
+        profilePicture: {
+            type: String,
+            default:
+                'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
+        },
+        isAdmin: {
+            type: Boolean,
+            default: false,
+        },
     },
-    password: {
-        type: String,
-        required: true,
-        maxLength: 20,
-        minLength: 8,
-    },
-    email: {
-        type: String,
-        unique: true,
-        required: true,
-    },
-});
+    { timestamps: true }
+);
