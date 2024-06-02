bcc3a6831a5645c8c3b57045283393b32184cbee
Alexander King Perocho <perocho.xela@gmail.com>
2024-06-01 23:17:44 -0400

    initialized server with mongo

--- src/server/src/index.mjs
+++ src/server/src/index.mjs
@@ -1,9 +1,13 @@
 import bodyParser from 'body-parser';
 import express from 'express';
 import mongoose from 'mongoose';
+import { MongoClient, ServerApiVersion } from 'mongodb';
 import cors from 'cors';
 
-app = express();
+const mongoUser = `${process.env.MONGO_USER}:${process.env.MONGO_PWD}`;
+const uri = `mongodb+srv://${mongoUser}@maritescluster.f0kk2lm.mongodb.net/?retryWrites=true&w=majority&appName=MaritesCluster`;
+
+const app = express();
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
