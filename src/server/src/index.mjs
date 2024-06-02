45a3b547350b42976c1c5b1a01dcc802a52898b9
Alexander King Perocho <perocho.xela@gmail.com>
2024-06-01 22:52:52 -0400

    init express

--- src/server/src/index.mjs
+++ src/server/src/index.mjs
@@ -2,3 +2,8 @@ import bodyParser from 'body-parser';
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+app = express();
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
