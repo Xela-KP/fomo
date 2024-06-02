9475555f7d8ceacac06ecb13685cf6ca78f4ec49
Alexander King Perocho <perocho.xela@gmail.com>
2024-06-02 13:10:28 -0400

    removed unused database

--- src/server/src/index.js
+++ src/server/src/index.js
@@ -1,34 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import { MongoClient, ServerApiVersion } from 'mongodb';
 
 dotenv.config();
 const app = express();
-
 const PORT = process.env.PORT;
-const URI = process.env.MONGO;
-console.log(URI);
-const CLIENT = new MongoClient(URI, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    },
-});
-
-const run = async () => {
-    try {
-        await CLIENT.connect();
-        app.listen(process.env.PORT);
-        await CLIENT.db('admin').command({ ping: 1 });
-        console.log(
-            'Pinged your deployment. You successfully connected to MongoDB!'
-        );
-    } catch (error) {
-        console.error(error);
-    } finally {
-        await CLIENT.close();
-    }
-};
-
-run().catch(console.dir);
