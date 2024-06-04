e7d54ae77f2c415e0fd1d68b8187d5b5b74359aa
Alexander King Perocho <perocho.xela@gmail.com>
2024-06-04 14:22:45 -0400

    added functionality to sign up

--- src/client/src/pages/Signup.tsx
+++ src/client/src/pages/Signup.tsx
@@ -1,16 +1,22 @@
-import { Label, Button, TextInput } from 'flowbite-react';
+import { Label, Button, TextInput, Alert, Spinner } from 'flowbite-react';
 import {
     ChangeEvent,
     ChangeEventHandler,
+    FormEvent,
+    FormEventHandler,
     MouseEventHandler,
     useState,
 } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default () => {
     const [showPwd, setShowPwd] = useState(false);
     const [pwdPrompt, setPwdPrompt] = useState('show');
     const [pwdType, setPwdType] = useState('password');
+    const [loading, setLoading] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
+    const [formData, setFormData] = useState({});
+    const navigate = useNavigate();
 
     const togglePwdPrompt: MouseEventHandler = () => {
         setShowPwd(!showPwd);
@@ -24,7 +30,42 @@ export default () => {
     };
 
     const handleInput: ChangeEventHandler = (e: ChangeEvent) => {
-        console.log(e.target);
+        const target: HTMLInputElement = e.target as HTMLInputElement;
+        setFormData({ ...formData, [target.id]: target.value.trim() });
+    };
+
+    const handleSubmit: FormEventHandler = async (e: FormEvent) => {
+        e.preventDefault();
+        const { username, email, password } = formData as {
+            username: string;
+            email: string;
+            password: string;
+        };
+        if (
+            !username ||
+            !email ||
+            !password ||
+            username === '' ||
+            email === '' ||
+            password === ''
+        )
+            return setErrorMessage('All fields are required');
+        setErrorMessage('');
+        try {
+            setLoading(true);
+            const req: RequestInit = {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(formData),
+            };
+            const res = await fetch('/api/auth/signup', req);
+            const data = await res.json();
+            if (data.success === false) return setErrorMessage(data.message);
+            navigate('../login');
+        } catch (error: any) {
+            setErrorMessage(error.message);
+        }
+        return setLoading(false);
     };
 
     return (
@@ -33,13 +74,13 @@ export default () => {
                 <span className="my-10 flex justify-center">
                     <Label className="text-xl">Create an account</Label>
                 </span>
-                <form className="flex flex-col gap-4">
+                <form onSubmit={handleSubmit} className="flex flex-col gap-4">
                     <div>
                         <Label value="Username" />
                         <TextInput
+                            id="username"
                             type="text"
                             placeholder="Username"
-                            id="username"
                             onChange={handleInput}
                         />
                     </div>
@@ -67,12 +108,21 @@ export default () => {
                         <TextInput
                             id="password"
                             type={pwdType}
+                            placeholder="Password"
                             required
                             onChange={handleInput}
                         />
                     </div>
-                    <Button type="submit" gradientDuoTone="greenToBlue">
-                        Sign Up
+                    <Button
+                        type="submit"
+                        gradientDuoTone="greenToBlue"
+                        disabled={loading}
+                    >
+                        {loading ? (
+                            <Spinner>Loading...</Spinner>
+                        ) : (
+                            <span>Sign Up</span>
+                        )}
                     </Button>
                 </form>
                 <div className="mt-2 space-x-2">
@@ -83,6 +133,11 @@ export default () => {
                         </Label>
                     </Link>
                 </div>
+                {errorMessage && (
+                    <Alert className="mt-5" color="failure">
+                        {errorMessage}
+                    </Alert>
+                )}
             </div>
         </div>
     );
