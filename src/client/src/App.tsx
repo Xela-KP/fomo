cd74d6a9ff83e8849d1152ca434acb7f23b2f239
Alexander King Perocho <perocho.xela@gmail.com>
2024-06-04 01:51:23 -0400

    set up routes

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,1 +1,13 @@
-export default () => <h1>Hello World</h1>;
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+export default () => (
+    <BrowserRouter>
+        <Routes>
+            <Route path="./pages/login" element={<Home />}></Route>
+            <Route path="./pages/login" element={<Signup />}></Route>
+            <Route path="./pages/login" element={<Login />}></Route>
+        </Routes>
+    </BrowserRouter>
+);
