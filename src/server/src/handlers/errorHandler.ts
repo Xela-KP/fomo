8d4706234c07980182d921bd93d2136cb7d02520
Alexander King Perocho <perocho.xela@gmail.com>
2024-06-17 00:23:20 -0400

    refactored error catch wrapper

--- src/server/src/handlers/errorHandler.ts
+++ src/server/src/handlers/errorHandler.ts
@@ -2,16 +2,25 @@ import { NextFunction, Request, Response } from 'express';
 
 import type { Middleware } from 'types/middleware';
 
-export const errorHandler =
-    (middleware: Middleware) =>
-    (req: Request, res: Response, next: NextFunction) => {
-        return middleware(req, res, next).catch((error: Error) => {
-            return res.status(400).send({
-                success: false,
-                result: null,
-                message: error.message,
-                controller: middleware.name,
-                error: error,
-            });
-        });
+export const catchError =
+    (middleware: Middleware): Middleware =>
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const result = middleware(req, res, next);
+            if (result instanceof Promise) {
+                await result;
+            }
+            return next();
+        } catch (error) {
+            if (error instanceof Error) {
+                return res.status(400).send({
+                    success: false,
+                    result: null,
+                    message: 'Required fields are not supplied',
+                    controller: middleware.name,
+                    error: error,
+                });
+            }
+            return next();
+        }
     };
