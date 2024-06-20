61b3b83d89e0f90ed920b0949a5d14243aa9393b
Alexander King Perocho <perocho.xela@gmail.com>
2024-06-20 02:40:11 -0400

    completed client side signup implementation

--- src/client/src/pages/auth/Signup.tsx
+++ src/client/src/pages/auth/Signup.tsx
@@ -1,85 +1,41 @@
 import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
-import type {
-    ChangeEvent,
-    ChangeEventHandler,
-    FormEvent,
-    FormEventHandler,
-    MouseEventHandler,
-} from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 
+import { Link, useNavigate } from 'react-router-dom';
 import { OAuthButton } from '@src/components/auth/OAuthButton';
-import { useState } from 'react';
+import type { AppDispatch, RootState } from '@src/redux/store';
+import type { SignupForm } from '@shared/forms/auth';
+import { useState, type ChangeEventHandler } from 'react';
+import { signup } from '@src/redux/auth/thunks';
 
 export const Signup = () => {
-    const [showPwd, setShowPwd] = useState(false);
-    const [pwdPrompt, setPwdPrompt] = useState('show');
-    const [pwdType, setPwdType] = useState('password');
-    const [loading, setLoading] = useState(false);
-    const [errorMessage, setErrorMessage] = useState<string>();
-    const [formData, setFormData] = useState({});
+    const { isLoading, errorMessage, isSuccess } = useSelector(
+        (state: RootState) => state.auth
+    );
+    const dispatch = useDispatch<AppDispatch>();
+    const [signupForm, setSignupForm]: [SignupForm, Function] = useState({
+        username: '',
+        email: '',
+        password: '',
+    });
     const navigate = useNavigate();
-
-    const togglePwdPrompt: MouseEventHandler = () => {
-        setShowPwd(!showPwd);
-        if (showPwd) {
-            setPwdPrompt('hide');
-            setPwdType('');
-        } else {
-            setPwdPrompt('show');
-            setPwdType('password');
-        }
+    const handleTextInput: ChangeEventHandler<HTMLInputElement> = (e) => {
+        setSignupForm({ ...signupForm, [e.target.id]: e.target.value });
     };
-
-    const handleInput: ChangeEventHandler = (e: ChangeEvent) => {
-        const target: HTMLInputElement = e.target as HTMLInputElement;
-        setFormData({ ...formData, [target.id]: target.value.trim() });
-    };
-
-    const handleSubmit: FormEventHandler = async (e: FormEvent) => {
-        e.preventDefault();
-        setErrorMessage(undefined);
-        const { username, password } = formData as {
-            username: string;
-            email: string;
-            password: string;
-        };
-        if (username.length < 3) {
-            setErrorMessage('Username must contain 3 characters or more');
-            return setLoading(false);
-        }
-        if (password.length < 8) {
-            setErrorMessage('Password must contain 8 characters or more');
-            return setLoading(false);
-        }
-        try {
-            setLoading(true);
-            const req: RequestInit = {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify(formData),
-            };
-            const res = await fetch('/api/signup', req);
-            const data = await res.json();
-            console.log(data);
-            if (data.success === true) navigate('../login');
-            else
-                setErrorMessage(
-                    'An account with the provided username or email already exists.'
-                );
-        } catch (error) {
-            console.log(error);
-        }
-        return setLoading(false);
-    };
-
     return (
         <div className="flex w-full h-full justify-center">
             <div className="m-4 max-w-lg w-full justify-center">
                 <span className="my-10 flex justify-center">
                     <Label className="text-xl">Create an account</Label>
                 </span>
-                <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+                <form
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                        dispatch(signup(signupForm));
+                        if (isSuccess) navigate('/login');
+                    }}
+                    className="flex flex-col gap-4"
+                >
                     <div>
                         <Label value="Username" />
                         <TextInput
@@ -87,44 +43,35 @@ export const Signup = () => {
                             type="text"
                             placeholder="Username"
                             required
-                            onChange={handleInput}
+                            onChange={handleTextInput}
                         />
                     </div>
                     <div>
-                        <div className="mb-2 block">
-                            <Label htmlFor="email" value="Email" />
-                        </div>
+                        <Label htmlFor="email" value="Email" />
                         <TextInput
                             id="email"
                             type="email"
                             placeholder="example@domain.com"
                             required
-                            onChange={handleInput}
+                            onChange={handleTextInput}
                         />
                     </div>
                     <div>
-                        <div className="mb-2 flex justify-between">
-                            <Label htmlFor="password" value="Password" />
-                            <Label
-                                value={pwdPrompt}
-                                onClick={togglePwdPrompt}
-                                className="p-1 cursor-pointer select-none"
-                            />
-                        </div>
+                        <Label htmlFor="password" value="Password" />
                         <TextInput
                             id="password"
-                            type={pwdType}
+                            type="password"
                             placeholder="********"
                             required
-                            onChange={handleInput}
+                            onChange={handleTextInput}
                         />
                     </div>
                     <Button
                         type="submit"
                         gradientDuoTone="greenToBlue"
-                        disabled={loading}
+                        disabled={isLoading}
                     >
-                        {loading ? (
+                        {isLoading ? (
                             <Spinner>Loading...</Spinner>
                         ) : (
                             <span>Sign Up</span>
@@ -140,7 +87,7 @@ export const Signup = () => {
                         </Label>
                     </Link>
                 </div>
-                {errorMessage && (
+                {!isSuccess && (
                     <Alert className="mt-5" color="failure">
                         {errorMessage}
                     </Alert>
